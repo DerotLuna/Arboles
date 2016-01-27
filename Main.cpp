dee7892db79551b32302bc6afa6cd0aab0c9d3ed
Ruben Luna <rubenlunamate@gmail.com>
2016-01-26 21:08:57 -0430

    Nuevo main

--- Main.cpp
+++ Main.cpp
@@ -1,59 +1,41 @@
-# Arboles
-ABB
 #include <stdio.h>
 #include <stdlib.h>
-#include "primitivas.h"
+#include "Header.h"
 
-void main()
-{
-	int op = -1, n = 0;
+void main() {
+	int  op = -1, n = 0;
 	nodo *p = NULL;
-
 	while (op != 0) {
 		system("cls");
-		printf("\n\n                              Menu principal Arboles! \n\n\n");
-		printf("\n 1. Insertar (x) en el arbol.");
-		printf("\n 2. Buscar (x) en el arbol."); 
-		printf("\n 3. Eliminar (x) del arbol.");
-		printf("\n 4. Impresion preorden.");
-		printf("\n 5. Impresion inorden."); 
-		printf("\n 6. Imprimir por niveles.");
-		printf("\n\n 0. Salir. \n\n");
-		printf(" Indique opcion : ");
+		printf("\n\n                                 Menu Principal \n");
+		printf("\n 1. Insertar. \n");
+		printf("\n 2. Mostrar en Preorden. \n");
+		printf("\n 3. Borrar. \n");
+		printf( "\n 4. Muestra la cuenta de : todos sus nodos, sus  internos y sus hojas. \n\n");
+		printf(" Salir : 0 \n\n");
+		printf("Indique opcion (1-4)");
 		op = 0;
 		scanf("%i", &op);
 		system("cls");
 		printf("\n\n");
-		switch (op)
-		{
-		case 1: printf("\n\n Indique el valor a ingresar : ");
+		switch (op) {
+		case 1: printf("\n\n1. Insertar un elemento \n\n");
+			printf("numero a insertar =");
 			scanf("%i", &n);
 			insertar(&p, n);
 			break;
-
-		 case 2: printf("\n\n Numero a buscar en el arbol:");
-			scanf("%i", &n);
-			buscar(p, n);
-			break;
-
-		 case 3: printf("\n\n Numero a eliminar del arbol :");
-			 scanf("%i", &n);
-			 eliminar(&p, n);
-			 break;
-
-		case 4: printf("\n\n Impresion Preorden \n\n ");
+		case 2: printf("\n\n2. Mostrar en PREorden\n\n ");
 			preorden(p);
 			break;
-
-		case 5: printf("\n\n Impresion Inorden \n\n ");
-			inorden(p);
+		case 3: printf("\n\n3. Eliminar un dato \n\n ");
+			printf("numero a eliminar =");
+			scanf("%i", &n);
+			eliminar(&p, n);
 			break;
-
-		case 6: printf("\n\n Impresion por niveles \n\n ");
+		case 4: printf("%i [Todos], %i [Internos], %i [Hijos]", contar(p), internos(p), hojas(p));
 			break;
 		}
-		if (op)
-		{
+		if (op) {
 			printf("\n\n");
 			system("pause");
 			system("cls");
